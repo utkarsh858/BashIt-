5d599c135a266f5244475a4a6a9a6faa2e12f47b
utkarsh858 <utkarsh858iitr@gmail.com>
2017-12-12 15:28:20 +0530

    made the implementations actually work

--- EasyCommiter.py
+++ EasyCommiter.py
@@ -1,18 +1,41 @@
 import sublime
 import sublime_plugin
-import subprocess
+import os
 
 
 class AddCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		bashCommand = "cd "+currentDir+";git add -A;"
-		process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-		output, error = process.communicate()
+		currentWindow = self.view.window()
+		currentDir = currentWindow.folders()[0]
+		os.chdir(currentDir)		
+		bashCommand = "git add -A"
+		process = os.system(bashCommand)
+		bashCommand = "mkdir pop"
+		os.system(bashCommand)
+		
+
+
 
 class CommitCommand(sublime_plugin.TextCommand):
+	commitmessage ="first"
+	currentDir=""
+
+	def on_change(self,string):
+		return
+
+	def on_done(self,string):
+		CommitCommand.commitmessage=string
+		os.chdir(self.currentDir)		
+
+		bashCommand = "git commit -m '"+self.commitmessage+"'"
+		process = os.system(bashCommand)
+
+	def on_cancel(self):
+		exit()
+
 	def run(self, edit):
+		currentWindow = self.view.window()
+		CommitCommand.currentDir = currentWindow.folders()[0]
+		currentWindow.show_input_panel("Commit message","Minor changes", self.on_done,self.on_change, self.on_cancel)
 
-		bashCommand = "cd "+currentDir+";git commit -m \""+commitmessage+"\";"
-		process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-		output, error = process.communicate()
-
+	
